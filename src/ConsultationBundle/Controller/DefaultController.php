a01b9d7e347eee0067d0998ee81288926a699dea
FZ <nakachfz@gmail.com>
2020-07-07 18:30:07 +0100

    final
    inchaallah

--- src/ConsultationBundle/Controller/DefaultController.php
+++ src/ConsultationBundle/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ConsultationBundle:Default:off.html.twig');
+        return $this->render('ConsultationBundle:Default:base.html.twig');
     }
 
  
